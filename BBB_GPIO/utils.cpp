cf3692ed45908bc4d0d16d0e9a88fcdc3e348c2a
Felipe Garcia <felipe_garcia99@hotmail.com>
2021-08-15 16:08:54 -0300

    feat: add sigaction functions

--- BBB_GPIO/utils.cpp
+++ BBB_GPIO/utils.cpp
@@ -1,6 +1,21 @@
 #include "utils.hpp"
 #include <unistd.h>
+#include <signal.h> //  our new library 
+
+volatile sig_atomic_t flag = 0;
 
 void delay(float time){
 	usleep(time * 1000000);
+}
+
+void my_handler(int signal){ // can be called asynchronously
+    flag = 1; // set flag
+}
+
+void defining_sigaction(){
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = my_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
 }
--- BBB_GPIO/utils.hpp
+++ BBB_GPIO/utils.hpp
@@ -1,1 +1,7 @@
+#include <signal.h> //  our new library 
+
+extern volatile sig_atomic_t flag;
+
 void delay(float time);
+void defining_sigaction();
+void my_handler(int signal);
