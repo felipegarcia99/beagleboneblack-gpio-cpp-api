dd9964a478a15857b6e0fe63e96785d97a7369b6
Felipe Garcia <felipe_garcia99@hotmail.com>
2021-08-18 23:15:28 -0300

    refactor: comment not implemented function

--- BBB_GPIO/digital.hpp
+++ BBB_GPIO/digital.hpp
@@ -9,5 +9,5 @@ void pinMode(int pin, string mode);
 void export_gpio(int pin);
 void direction_definer(int pin, string mode);
 void digitalWrite(int pin, int state);
-void digitalRead(int pin);
+//void digitalRead(int pin); //soon...
 void unexport_gpio(int pin);
